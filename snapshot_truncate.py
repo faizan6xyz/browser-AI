7fe10198491cf4487d4579416f188d5714fc9b0b
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-07-08 18:33:30 +0530

    corerection of the snapshot

--- snapshot_truncate.py
+++ snapshot_truncate.py
@@ -1,12 +1,10 @@
 def read_yaml_lines(filepath):
-    """Read YAML file line by line"""
     with open(filepath, 'r',encoding='utf-8') as file:
         lines = file.readlines()
     return [line.strip() for line in lines if line.strip()]
-
-# Example usage
-lines = read_yaml_lines('.playwright-mcp/page-2026-07-07T23-56-39-831Z.yml')
-for i, line in enumerate(lines, 1):
-    if "[cursor=pointer]" in line :
-        print(f"Line {i}: {line}")
-    
+def fileread (name):
+    lines = read_yaml_lines(name)
+    for i, line in enumerate(lines, 1):
+        if "[cursor=pointer]" in line :
+            print(f"Line {i}: {line}")
+        
