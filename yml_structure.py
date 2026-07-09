25e051d5edede0373c58fb6e091fc57210262dd6
faizan6xyz <232054644+faizan6xyz@users.noreply.github.com>
2026-07-10 05:04:59 +0530

    user name extracted

--- yml_structure.py
+++ yml_structure.py
@@ -1,9 +1,19 @@
+import re
 def new(filename):
-    if not  filename :
+    listt=[]
+    if not  filename :  
         print("empty snapshot")
     with open(filename , "r", encoding="utf-8") as f :
         lis = [item for item in f.readlines()]
-    return lis
+    for item in lis :        
+        if 'link' in item and 'profile picture' in item and '[cursor=pointer]' in item and "dontstockbitch" not in item:
+            pattern = r'link \"([^"]+)\''
+            match = re.search(pattern, item)
+            if match:
+                full_content = match.group(1) 
+                username = full_content.split("'s")[0]
+                listt.append(username)
+    return listt
         
         
 print(new(r".playwright-mcp/page-2026-07-09T22-41-34-999Z.yml"))
